b0db245b42cb20fe01a84192c3f43fcf53fdbca5
EloiGry <eloi.grychta@gmail.com>
2021-11-16 17:30:45 +0100

    modif

--- src/Pages/Favorites.js
+++ src/Pages/Favorites.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Card from '../components/Card';
 
 class Favorites extends Component {
     constructor() {
@@ -9,19 +10,36 @@ class Favorites extends Component {
         }
     }
 
+    componentDidMount () {
+        this.state.favIDs.forEach(id => {
+            this.getMovie(id)
+        });
+    }
+
     getStorage () {
         let favorites = JSON.parse(localStorage.getItem("favorites"))
-        console.log("hello", favorites)
+        return favorites
     }
 
     getMovie (id) {
-
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=74ff4d5b18f55c304a239fadf716fe2f`)
+          .then(response => response.json())
+          .then(result => {
+              this.setState({ movies: [result, ...this.state.movies]})
+            })
     }
-    
+
     render() {
+        const {movies} = this.state
         return (
             <div>
                 <h1> Favorites </h1>
+                {movies.map(movie => 
+                    <Card 
+                    title={movie.title} 
+                    release_date={movie.release_date} 
+                    overview={movie.overview} 
+                    poster_path={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}/>)} 
             </div>
         );
     }
--- src/Pages/Weekly.js
+++ src/Pages/Weekly.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import Card from '../components/Card';
+import moment from 'moment'
 
-class Weekly extends Component {
+class Popular extends Component {
+    constructor () {
+        super()
+        this.state = {
+            movies : [],
+        }
+    }
+
+    componentDidMount() {
+        fetch(`http://api.themoviedb.org/3/discover/movie?primary_release_date.gte=${this.moment()}&primary_release_date.lte=${this.moment()}&api_key=74ff4d5b18f55c304a239fadf716fe2f`)
+          .then(response => response.json())
+          .then(result => {
+            this.setState({ movies: result.results })
+          })
+      }
     render() {
+        const { movies } = this.state
+        console.log(movies);
         return (
-            <div>
-                <h1> Weekly </h1>
+            <div className="container">
+                <h1> Popular </h1>
+                <div className="row"> 
+                    {movies.map(movie => 
+                        <Card title={movie.title} release_date={movie.release_date} overview={movie.overview} poster_path={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}/>)}
+                </div>
             </div>
         );
     }
 }
 
-export default Weekly;
+export default Popular;
